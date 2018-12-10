e3118ca7101439eb41ca0a580658bd9533c22e14
Alexandr Suvorov <al3xandr.suvorow@gmail.com>
2018-12-10 18:30:33 +0400

    Revert "Revert "Added feature to open photo from list and scale/drag it, using PhotoView lib""
    This reverts commit 535bbbc619ee573bca3fd17b17873faac0741379.

--- app/src/main/java/com/example/alex/nasacuriosity/common/PhotosAdapter.kt
+++ app/src/main/java/com/example/alex/nasacuriosity/common/PhotosAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.alex.nasacuriosity.common
 
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,36 +9,48 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.alex.nasacuriosity.R
+import com.example.alex.nasacuriosity.activity.ZoomedImageActivity
 import com.example.alex.nasacuriosity.db.entity.Photo
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.cardview.view.photo_title
-import kotlinx.android.synthetic.main.cardview.view.photo_view
+import kotlinx.android.synthetic.main.cardview.view.*
 
 class PhotosAdapter(
-  private val photos: List<Photo>
+    private val photos: List<Photo>,
+    private var onItemClickListener: View.OnClickListener? = null
 ) : RecyclerView.Adapter<PhotosAdapter.PhotoViewHolder>() {
 
-  override fun getItemCount(): Int = photos.size
+    override fun getItemCount(): Int = photos.size
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-    val view = LayoutInflater.from(parent.context).inflate(R.layout.cardview, parent, false)
-    return PhotoViewHolder(view)
-  }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.cardview, parent, false)
+        return PhotoViewHolder(view).withListener { pos, _ ->
+            val intent = Intent(parent.context, ZoomedImageActivity::class.java)
+            intent.putExtra("Image_URL", photos[pos].imageUrl)
+            parent.context.startActivity(intent)
+        }
+    }
 
-  override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-   /* val photoId = photos[position].id
-    holder.photoId.text = photoId.toString()*/
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        /* val photoId = photos[position].id
+         holder.photoId.text = photoId.toString()*/
 
-    val photoImage = photos[position].imageUrl
-    Picasso.get()
-      .load(photoImage)
-      .placeholder(R.drawable.placeholder)
-      .into(holder.photoImage)
-  }
+        val photoImage = photos[position].imageUrl
+        Picasso.get()
+            .load(photoImage)
+            .placeholder(R.drawable.placeholder)
+            .into(holder.photoImage)
+    }
 
-  class PhotoViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-    val photoImage: ImageView = view.photo_view
-    val photoId: TextView = view.photo_title
-  }
+    class PhotoViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val photoImage: ImageView = view.photo_view
+        val photoId: TextView = view.photo_title
+    }
+
+    fun <T : RecyclerView.ViewHolder> T.withListener(event: (position: Int, type: Int) -> Unit): T {
+        itemView.setOnClickListener {
+            event.invoke(adapterPosition, itemViewType)
+        }
+        return this
+    }
 }
 
